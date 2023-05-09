b98fe325692ac349762569e3c48a7bd07b28260f
boywonder3 <opeyemiisaacboywonder@gmail.com>
2023-05-09 10:10:12 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles.css";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [newTodoName, setNewTodoName] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function addNewTodo() {
+    if(newTodoName === "") return 
+    
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { name: newTodoName, completed: false, id: crypto.randomUUID() },
+      ];
+    });
+    setNewTodoName('')
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div id="new-todo-form">
+    {JSON.stringify(todos)}
+      <label for="todo-input">New Todo</label>
+      <input
+        type="text"
+        id="todo-input"
+        value={newTodoName}
+        onChange={(e) => setNewTodoName(e.target.value)}
+      />
+      <button onClick={addNewTodo}>Add Todo</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
